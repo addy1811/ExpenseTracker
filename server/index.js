e9951f8ba96921f8b1150291633e5461f3fdaa14
Aditya Mewar <adityamewar061@gmail.com>
2026-06-11 11:22:06 +0530

    Day 4: update CORS for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -8,7 +8,12 @@ import { notFound , errorCheck } from "./middleware/errorCheck.js";
 dotenv.config({quiet : true} );
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        process.env.LOCAL_URL,
+        process.env.VERCEL_URL
+    ]
+}));
 app.use(express.json());
 
 app.get("/", (req , res) => {
@@ -21,7 +26,7 @@ app.use("/api/expenses", expenseRoutes);
 app.use(notFound);
 app.use(errorCheck);
 
-const PORT = process.env.ENV_PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT , () => {
     console.log(`Server running on port ${PORT}`);
